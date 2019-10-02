7e03035f1cde20f97673fb2c1c70ec9a4cecbd1d
stardiviner <numbchild@gmail.com>
2019-10-02 12:21:38 +0800

    add init-emacs-overlay

---
+++ init/Emacs/init-emacs-overlay.el
@@ -0,0 +1,21 @@
+;;; init-emacs-overlay.el --- init for Emacs overlay
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ overlay ]
+
+;;; [ ov ] -- Overlay library for Emacs Lisp.
+
+(use-package ov
+  :ensure t
+  :defer t)
+
+
+(provide 'init-emacs-overlay)
+
+;;; init-emacs-overlay.el ends here
