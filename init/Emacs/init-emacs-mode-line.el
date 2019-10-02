de41cd6166d9753ce0870e6107a2d2672c37c816
stardiviner <numbchild@gmail.com>
2019-10-02 13:43:36 +0800

    disable doom-modeline.

--- init/Emacs/init-emacs-mode-line.el
+++ init/Emacs/init-emacs-mode-line.el
@@ -20,15 +20,15 @@
 
 ;;; [ doom-modeline ] -- A minimal and modern mode-line.
 
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode)
-  :init (setq doom-modeline-buffer-file-name-style 'buffer-name
-              doom-modeline-icon t ; don't use icon will be faster
-              doom-modeline-github nil
-              doom-modeline-irc nil
-              ;; Fix the laggy issue, by don't compact font caches during GC.
-              inhibit-compacting-font-caches t))
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :hook (after-init . doom-modeline-mode)
+;;   :init (setq doom-modeline-buffer-file-name-style 'buffer-name
+;;               doom-modeline-icon t ; don't use icon will be faster
+;;               doom-modeline-github nil
+;;               doom-modeline-irc nil
+;;               ;; Fix the laggy issue, by don't compact font caches during GC.
+;;               inhibit-compacting-font-caches t))
 
 
 (provide 'init-emacs-mode-line)
