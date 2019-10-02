c0f3684703ba9aa1c61fb8c19774399ab9190194
stardiviner <numbchild@gmail.com>
2019-10-02 12:29:07 +0800

    remove incompatible init-emacs-overlay

--- init.el
+++ init.el
@@ -85,7 +85,6 @@
 (require 'init-emacs-search)
 (require 'init-emacs-highlight)
 (require 'init-emacs-regex)
-(require 'init-emacs-overlay)
 (require 'init-emacs-vcs)
 (require 'init-emacs-shell)
 (require 'init-emacs-comint)
